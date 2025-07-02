ba67a7f570c0b71db771f9fdb209a809960cc59d
Juwon Cha <littlebigcha@gmail.com>
2025-07-02 10:07:08 +0900

    타이틀 화면 애니메이션 재생 오류 수정

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    public void Start()
+    {
+        // 애니메이션 재생을 위해 타임 스케일 1로 설정
+        Time.timeScale = 1f;
+    }
+
     public void OnClickLevelSelctBtn()
     {
         Debug.Log("Load Level Select Scene");
